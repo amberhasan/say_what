fff210786c1d97cb2accaf767020e728d474df74
Amber Hasan <amber.hasan@gmail.com>
2023-11-04 04:49:05 -0500

    Remove headers

--- App.tsx
+++ App.tsx
@@ -12,39 +12,34 @@ import SearchScreen from './src/screens/SearchScreen';
 
 const Tab = createBottomTabNavigator();
 const DiscoverStack = createStackNavigator();
-
-function DiscoverStackNavigator() {
-  return (
-    <DiscoverStack.Navigator initialRouteName="DiscoverScreen">
-      <DiscoverStack.Screen
-        name="DiscoverScreen"
-        component={DiscoverScreen}
-        options={{title: 'Discover'}} // This will set the title in the header
-      />
-      <DiscoverStack.Screen
-        name="CategoriesScreen"
-        component={CategoriesScreen}
-        options={{title: 'Location'}} // This will set the title in the header
-      />
-      <DiscoverStack.Screen
-        name="CaptionsScreen"
-        component={CaptionsScreen}
-        options={{title: 'Mountain'}} // This will set the title in the header
-      />
-    </DiscoverStack.Navigator>
-  );
-}
-
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={{headerShown: false}} // This will hide the header for all screens in the Tab navigator
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Discover" component={DiscoverStackNavigator} />
         <Tab.Screen name="Favorites" component={FavoritesScreen} />
         <Tab.Screen name="Search" component={SearchScreen} />
       </Tab.Navigator>
     </NavigationContainer>
+  );
+}
+
+function DiscoverStackNavigator() {
+  return (
+    <DiscoverStack.Navigator
+      initialRouteName="DiscoverScreen"
+      screenOptions={{headerShown: false}} // This will hide the header for all screens in the Discover stack
+    >
+      <DiscoverStack.Screen name="DiscoverScreen" component={DiscoverScreen} />
+      <DiscoverStack.Screen
+        name="CategoriesScreen"
+        component={CategoriesScreen}
+      />
+      <DiscoverStack.Screen name="CaptionsScreen" component={CaptionsScreen} />
+    </DiscoverStack.Navigator>
   );
 }
 
