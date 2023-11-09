ce18e435d1b1ad4912d8631f71632c09fe98edf5
Amber Hasan <amber.hasan@gmail.com>
2023-11-09 00:04:02 -0600

    Added fonts

--- src/screens/CaptionsScreen.tsx
+++ src/screens/CaptionsScreen.tsx
@@ -5,46 +5,71 @@ import {
   StyleSheet,
   FlatList,
   TouchableOpacity,
-  Modal,
-  Button,
   Clipboard,
-  Alert, // Import Alert to provide feedback
+  Image,
+  Alert,
 } from 'react-native';
-import captionsData from '../data/captions/captionsData'; // Import the data
+import captionsData from '../data/captions/captionsData';
 import {useDispatch} from 'react-redux';
-import {addToFavorites as addToFavoritesAction} from '../actions/favoritesActions'; // adjust the path as necessary
+import {addToFavorites as addToFavoritesAction} from '../actions/favoritesActions';
 
 const CaptionsScreen = ({route}) => {
   const {selectedCategory} = route.params;
   const phrases = captionsData[selectedCategory] || [];
   const [selectedCaption, setSelectedCaption] = useState(null);
-  const [modalVisible, setModalVisible] = useState(false);
+  const [dropdownVisible, setDropdownVisible] = useState(false);
   const dispatch = useDispatch();
 
   const copyToClipboard = text => {
     Clipboard.setString(text);
-    setModalVisible(false);
+    setDropdownVisible(false);
     Alert.alert('Copied!', 'Caption copied to clipboard.');
   };
 
   const addToFavorites = caption => {
-    dispatch(addToFavoritesAction(caption)); // Dispatch the action to add to favorites
-    setModalVisible(false);
+    dispatch(addToFavoritesAction(caption));
+    setDropdownVisible(false);
     Alert.alert('Added!', 'Caption added to favorites.');
   };
 
   const renderCaptionItem = ({item}) => {
     const isSelected = item === selectedCaption;
+
+    // Determine if the item is selected to change the text color
+    const itemStyle = isSelected ? styles.selectedItem : styles.listItem;
+
     return (
-      <TouchableOpacity
-        onPress={() => {
-          setSelectedCaption(item);
-          setModalVisible(true);
-        }}>
-        <Text style={[styles.listItem, isSelected && styles.selectedItem]}>
-          {`\u2022 ${item}`}
-        </Text>
-      </TouchableOpacity>
+      <View style={styles.captionContainer}>
+        <TouchableOpacity
+          onPress={() => {
+            setSelectedCaption(isSelected ? null : item); // Toggle the selection
+            setDropdownVisible(!dropdownVisible);
+          }}>
+          <Text style={itemStyle}>{`\u2022 \"${item}\"`}</Text>
+        </TouchableOpacity>
+        {dropdownVisible && isSelected && (
+          <View style={styles.dropdown}>
+            <TouchableOpacity
+              onPress={() => copyToClipboard(item)}
+              style={styles.dropdownItem}>
+              <Image
+                source={require('../assets/images/utils/link.png')}
+                style={styles.iconStyle}
+              />
+              <Text style={styles.textItem}>Copy to clipboard</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => addToFavorites(item)}
+              style={styles.dropdownItem}>
+              <Image
+                source={require('../assets/images/bottom/unselected/heart.png')}
+                style={styles.iconStyle}
+              />
+              <Text style={styles.textItem}>Add to favorites</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
     );
   };
 
@@ -55,90 +80,63 @@ const CaptionsScreen = ({route}) => {
         data={phrases}
         renderItem={renderCaptionItem}
         keyExtractor={(item, index) => index.toString()}
+        extraData={selectedCaption} // Adding this to ensure FlatList updates when state changes
       />
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
-        }}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Button
-              title="Copy to clipboard"
-              onPress={() => copyToClipboard(selectedCaption)}
-            />
-            <Button
-              title="Add to favorites"
-              onPress={() => addToFavorites(selectedCaption)}
-            />
-            <Button title="Cancel" onPress={() => setModalVisible(false)} />
-          </View>
-        </View>
-      </Modal>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   captionContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-    fontFamily: 'PlayfairDisplay-Regular',
+    flexDirection: 'column',
   },
-  bulletPoint: {
-    width: 10,
-    height: 10,
-    borderRadius: 5,
-    backgroundColor: 'black', // Change this to match your desired bullet point color
-    marginRight: 10,
+  listItem: {
+    fontSize: 20,
     fontFamily: 'PlayfairDisplay-Regular',
+    color: 'black', // Default color for unselected item
+  },
+  selectedItem: {
+    fontSize: 20,
+    fontFamily: 'PlayfairDisplay-Bold',
+    color: '#FF66C3', // Pink color for selected item
+  },
+  textItem: {
+    fontSize: 15,
+    fontFamily: 'OpenSans-Regular',
+    color: 'black',
+  },
+  iconStyle: {
+    width: 20, // Set the width as needed
+    height: 20, // Set the height as needed
+    marginRight: 10, // Add some spacing between the icon and the text
+  },
+  // Modify the dropdownItem to handle the layout with an icon
+  dropdownItem: {
+    flexDirection: 'row', // Align icon and text horizontally
+    alignItems: 'center', // Center items vertically
+    paddingVertical: 2,
+    fontSize: 18,
+    color: 'black',
+    borderColor: '#eee',
+  },
+  dropdown: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+    // Removed shadow styles
+    elevation: 0, // Set elevation to 0 to remove shadow on Android
   },
   container: {
     flex: 1,
     padding: 20,
     paddingTop: 100,
     backgroundColor: 'white',
-    fontFamily: 'PlayfairDisplay-Regular',
   },
   title: {
     fontSize: 32,
     fontWeight: 'bold',
     marginBottom: 20,
     textAlign: 'center',
-    fontFamily: 'PlayfairDisplay-Regular',
-  },
-  listItem: {
-    fontSize: 20,
-    fontFamily: 'PlayfairDisplay-Regular',
-  },
-  selectedItem: {
-    color: '#FF66C3', // Change this to your preferred shade of pink
-    fontFamily: 'PlayfairDisplay-Regular',
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-    fontFamily: 'PlayfairDisplay-Regular',
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
     fontFamily: 'PlayfairDisplay-Regular',
   },
 });
